41bf4ebe78703afcffede64de23c8e78ff538979
litsynp <nocte55is@gmail.com>
2021-03-25 15:19:29 +0900

    feat: update index with test URLs

--- apps/festival/views.py
+++ apps/festival/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Hello, world! You\'re at the index.')
+    template = loader.get_template('festival/index.html')
+    context = {
+        'contents': [
+            {
+                'name': 'Test YouTube Link that pops up',
+                'popup': True,
+                'url': 'https://www.youtube.com/embed/TNsSBhl_2LI',
+            },
+            {
+                'name': 'Test YouTube Link 2 on current tab',
+                'popup': False,
+                'url': 'https://www.youtube.com/embed/TNsSBhl_2LI',
+            },
+        ],
+    }
+    return HttpResponse(template.render(context, request))
