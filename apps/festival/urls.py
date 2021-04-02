0343d6c1bf0a94c8f5d0f0e942dac49ec7cb324b
litsynp <nocte55is@gmail.com>
2021-04-03 00:56:54 +0900

    fix: change URL schemes

--- apps/festival/urls.py
+++ apps/festival/urls.py
@@ -7,12 +7,12 @@ app_name = 'festival'
 urlpatterns = [
     path('', views.index, name='index'),
     # path('cau-image/', views.cau_image_page, name='cau_image_page'),
-    path('escape_room/', views.escape_room, name='escape_room'),
-    path('finding_puang/', views.finding_puang, name='finding_puang'),
-    path('humanright_test/', views.humanright_test, name='humanright_test'),
-    path('mixmusic_quiz/', views.mixmusic_quiz, name='mixmusic_quiz'),
-    path('photo_puzzle/', views.photo_puzzle, name='photo_puzzle'),
+    path('escape-room/', views.escape_room, name='escape_room'),
+    path('finding-puang/', views.finding_puang, name='finding_puang'),
+    path('humanright-test/', views.humanright_test, name='humanright_test'),
+    path('mixmusic-quiz/', views.mixmusic_quiz, name='mixmusic_quiz'),
+    path('photo-puzzle/', views.photo_puzzle, name='photo_puzzle'),
     path('trickywords/', views.trickywords, name='trickywords'),
-    path('whereareyou_chungang/', views.whereareyou_chungang,
+    path('whereareyou-chungang/', views.whereareyou_chungang,
          name='whereareyou_chungang'),
 ]
