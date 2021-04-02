04ff655109a6b4e59ac3d71497946ba7a8e3e98b
litsynp <nocte55is@gmail.com>
2021-04-03 00:19:04 +0900

    feat: add 7 requested pages

--- apps/festival/views.py
+++ apps/festival/views.py
@@ -8,51 +8,51 @@ def index(request):
     context = {
         'contents': [
             {
-                'name': 'Test',
+                'name': '방탈출',
                 'popup': False,
-                'url': reverse('festival:cau_image_page'),
+                'url': reverse('festival:escape_room'),
                 'x': 1025,
                 'y': 180,
             },
             {
-                'name': 'Test',
+                'name': '푸앙이를 찾아라',
                 'popup': False,
-                'url': 'https://www.cau.ac.kr/',
+                'url': reverse('festival:finding_puang'),
                 'x': 830,
                 'y': 250,
             },
             {
-                'name': 'Test',
+                'name': '나의 교내 인권 상식 테스트',
                 'popup': False,
-                'url': 'https://www.cau.ac.kr/',
+                'url': reverse('festival:humanright_test'),
                 'x': 1025,
                 'y': 400,
             },
             {
-                'name': 'Test',
+                'name': '믹스음악퀴즈',
                 'popup': False,
-                'url': 'https://www.cau.ac.kr/',
+                'url': reverse('festival:mixmusic_quiz'),
                 'x': 640,
                 'y': 470,
             },
             {
-                'name': 'Test',
+                'name': '틀린그림찾기',
                 'popup': False,
-                'url': 'https://www.cau.ac.kr/',
+                'url': reverse('festival:photo_puzzle'),
                 'x': 1035,
                 'y': 610,
             },
             {
-                'name': 'Test',
+                'name': '말난장찾기',
                 'popup': False,
-                'url': 'https://www.cau.ac.kr/',
+                'url': reverse('festival:trickywords'),
                 'x': 500,
                 'y': 700,
             },
             {
-                'name': 'Test',
+                'name': '중앙아 어딨니?',
                 'popup': False,
-                'url': 'https://www.cau.ac.kr/',
+                'url': reverse('festival:whereareyou_chungang'),
                 'x': 775,
                 'y': 720,
             },
@@ -63,5 +63,68 @@ def index(request):
 
 def cau_image_page(request):
     template = loader.get_template('festival/cau_image_page.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def escape_room(request):
+    """
+    방탈출
+    """
+    template = loader.get_template('festival/escape_room.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def finding_puang(request):
+    """
+    푸앙이를 찾아라
+    """
+    template = loader.get_template('festival/finding_puang.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def humanright_test(request):
+    """
+    나의 교내 인권 상식 테스트
+    """
+    template = loader.get_template('festival/humanright_test.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def mixmusic_quiz(request):
+    """
+    믹스음악퀴즈
+    """
+    template = loader.get_template('festival/mixmusic_quiz.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def photo_puzzle(request):
+    """
+    틀린그림찾기
+    """
+    template = loader.get_template('festival/photo_puzzle.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def trickywords(request):
+    """
+    말난장찾기
+    """
+    template = loader.get_template('festival/trickywords.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def whereareyou_chungang(request):
+    """
+    중앙아 어딨니?
+    """
+    template = loader.get_template('festival/whereareyou_chungang.html')
     context = {}
     return HttpResponse(template.render(context, request))
