035da8e55236d8685bf763bfd7c7efe3979cf965
litsynp <nocte55is@gmail.com>
2021-04-02 13:42:10 +0900

    fix: register festival app in urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.festival.urls')),
+    path('', include('apps.festival.urls', namespace='festival')),
 ]
